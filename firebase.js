26110f1d107627ea8f5cf634b6f24227296e78ad
eugene44-hub <eugeneishado44@gmail.com>
2021-12-18 15:36:53 +0100

    fixed firebase and nav section

--- firebase.js
+++ firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider, signInWithPopup, FacebookAuthProvider, signOut } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAKOFyOF1eFEUTLmzzfhO_7NpVXz6lMCFw",
     authDomain: "cafe-auth-536ca.firebaseapp.com",
@@ -13,20 +9,16 @@ const firebaseConfig = {
     messagingSenderId: "723872785332",
     appId: "1:723872785332:web:d99eca19538d9dd47b88e1"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+const fbprovider = new FacebookAuthProvider();
+const googleBtn = document.querySelector("#gmailSignIn");
+const facebookBtn = document.querySelector("#fbSignIn");
 
-const googleBtn = document.querySelector("#gmailSignIn")
-googleBtn.addEventListener("click", e => {
-    const provider = new GoogleAuthProvider();
-    // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    // provider.setCustomParameters({
-    //     'login_hint': 'user@example.com'
-    // })
-    const auth = getAuth();
-    // auth.languageCode = 'it';
+const auth = getAuth();
 
+
+googleBtn.addEventListener('click', e => {
     signInWithPopup(auth, provider)
         .then((result) => {
             // This gives you a Google Access Token. You can use it to access the Google API.
@@ -34,9 +26,11 @@ googleBtn.addEventListener("click", e => {
             const token = credential.accessToken;
             // The signed-in user info.
             const user = result.user;
-            // ...
+            console.log(result.user.displayName);
+            window.location.href = "/homepage.html"
+                // ...
         }).catch((error) => {
-            // Handle Errors here.
+            // Handle Errors here.gnout
             const errorCode = error.code;
             const errorMessage = error.message;
             // The email of the user's account used.
@@ -45,4 +39,43 @@ googleBtn.addEventListener("click", e => {
             const credential = GoogleAuthProvider.credentialFromError(error);
             // ...
         });
+    // window.location.href = "/homepage.html"
+})
+
+facebookBtn.addEventListener('click', e => {
+    signInWithPopup(auth, fbprovider)
+        .then((result) => {
+            // The signed-in user info.
+            const user = result.user;
+
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            const credential = FacebookAuthProvider.credentialFromResult(result);
+            const accessToken = credential.accessToken;
+            window.location.href = "/homepage.html"
+
+            // ...
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = FacebookAuthProvider.credentialFromError(error);
+
+            // ...
+        });
+
+
+})
+
+const logOut = document.querySelector("#signOut")
+
+logOut.addEventListener('click', e => {
+    signOut(auth).then(() => {
+        // Sign-out successful.
+    }).catch((error) => {
+        // An error happened.
+    });
 })
--- js/header.js
+++ js/header.js
@@ -4,7 +4,7 @@ const markupHeader = `
         <div class="container">
             <a class="navbar-brand fw-bold fs-4" href="index.html"><img src="images/logo.webp" alt=""></a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
                 <span class="fw-bold">MENU</span>  <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse text-center text-md-start" id="navmenu">
@@ -24,24 +24,31 @@ const markupHeader = `
                     <li class="nav-item mx-lg-2  p-2 p-lg-0 hover-border">
                         <a href="contact.html" class="nav-link fw-bold text-dark">Contact</a>
                     </li>
+                   
                 </ul>
 
             </div>
-            <ul class="navbar-nav  ms-auto fw-bold fs-5">
-                <li class="nav-item mx-lg-2  p-2 p-lg-0 ">
-                    <div class="btn-dark border-0 nav-item fw-bold rounded-pill btn btn-light  btn-lg position-relative" style="background-color: #f86011;">
-                        +1023682802
-                        <div class="position-absolute  bg-dark rounded-pill " id="button-bg"></div>
-                    </div>
-                </li>
-                <li class="nav-item mx-lg-2  mt-2 p-2 p-lg-0 text-center">
-                    <div class="nav-item hover-border" id="book-table">
-                        Book a Table
-                    </div>
-                </li>
-            </ul>
+            
         </div>
     </nav>
+    <div class="d-flex  justify-content-end " style=" background-color: #fff4f2;">
+    <div class="container d-flex justify-content-end">
+    <ul class="navbar-nav fw-bold  fs-5" >
+    <li class="nav-item    p-lg-0 ">
+        <div class="btn-dark border-0 nav-item fw-bold rounded-pill btn btn-light  btn-lg position-relative" style="background-color: #f86011;">
+            +1023682802
+            <div class="position-absolute  bg-dark rounded-pill " id="button-bg"></div>
+        </div>
+    </li>
+    <li class="nav-item  mt-4 p-lg-0 text-center">
+        <div class="nav-item hover-border" id="book-table">
+            Book a Table
+        </div>
+    </li>
+</ul></div>
+  
+    </div>
+   
 `;
 
 const header = document.querySelector('.header');
