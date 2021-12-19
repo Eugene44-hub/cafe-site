81dfccf68d38f8c602f33bcedcc43e40c8f8443f
eugene44-hub <eugeneishado44@gmail.com>
2021-12-19 01:33:01 +0100

    alphabet on nav

--- firebase.js
+++ firebase.js
@@ -17,20 +17,26 @@ const facebookBtn = document.querySelector("#fbSignIn");
 
 const auth = getAuth();
 
+const signingIn = () => {
+
+}
+
 
 googleBtn.addEventListener('click', e => {
     signInWithPopup(auth, provider)
         .then((result) => {
+
             // This gives you a Google Access Token. You can use it to access the Google API.
             const credential = GoogleAuthProvider.credentialFromResult(result);
             const token = credential.accessToken;
             // The signed-in user info.
             const user = result.user;
-            console.log(result.user.displayName);
+            console.log(user.displayName);
             window.location.href = "/homepage.html"
-                // ...
+
+            // ...
         }).catch((error) => {
-            // Handle Errors here.gnout
+            // Handle Errors here.
             const errorCode = error.code;
             const errorMessage = error.message;
             // The email of the user's account used.
@@ -79,3 +85,15 @@ logOut.addEventListener('click', e => {
         // An error happened.
     });
 })
+
+// const user = auth.currentUser;
+
+// if (user !== null) {
+//     user.providerData.forEach((profile) => {
+//         console.log("Sign-in provider: " + profile.providerId);
+//         console.log("  Provider-specific UID: " + profile.uid);
+//         console.log("  Name: " + profile.displayName);
+//         console.log("  Email: " + profile.email);
+//         console.log("  Photo URL: " + profile.photoURL);
+//     });
+// }
--- js/header.js
+++ js/header.js
@@ -1,3 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+// import { auth } from "/Users/EUGENE/Desktop/CAFE/cafe-site/firebase"
+
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAKOFyOF1eFEUTLmzzfhO_7NpVXz6lMCFw",
+    authDomain: "cafe-auth-536ca.firebaseapp.com",
+    projectId: "cafe-auth-536ca",
+    storageBucket: "cafe-auth-536ca.appspot.com",
+    messagingSenderId: "723872785332",
+    appId: "1:723872785332:web:d99eca19538d9dd47b88e1"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+// const user = auth.currentUser;
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        console.log(user.photoURL, user.displayName, user.email)
+        const alpha = document.querySelectorAll("#alpha");
+        for (let i = 0; i < alpha.length; i++) {
+            alpha[i].innerHTML = `<span class="text-decoration-none">${user.displayName[0]}</span>`
+        } // ...
+    } else {
+        // User is signed out
+        // ...
+    }
+});
+
 const markupHeader = `
 	<nav class="navbar navbar-light navbar-expand-lg p-4" style="z-index: 1; background-color: #fff4f2;" id="top">
 
@@ -25,16 +58,19 @@ const markupHeader = `
                     <li class="nav-item mx-lg-2  p-2 p-lg-0 hover-border">
                         <a href="contact.html" class="nav-link fw-bold text-dark">Contact</a>
                     </li>
-                   
+                    <li class="mb-2">  <a href="#" class="text-decoration-none d-lg-none"> <div class="rounded-circle text-light   ms-auto justify-content-center align-items-center d-flex" style="width:45px;height: 45px; background-color: lightpink; font-size:23px;" id="alpha"></div></a></li>
                 </ul>
 
             </div>
             
-        </div>
+      <a href="#" class="text-decoration-none d-none d-lg-block"> <div class="rounded-circle text-light  justify-content-center align-items-center d-flex" style="width:45px;height: 45px; background-color: lightpink; font-size:23px;" id="alpha"></div></a>
+        
+            </div>
     </nav>
     <div class="d-flex  justify-content-end " style=" background-color: #fff4f2;">
     <div class="container d-flex justify-content-end">
     <ul class="navbar-nav fw-bold  fs-5" >
+   
     <li class="nav-item p-lg-0 ">
         <div class="btn-dark border-0 nav-item fw-bold rounded-pill btn btn-light  btn-lg position-relative" style="background-color: #f86011;">
             +1023682802
